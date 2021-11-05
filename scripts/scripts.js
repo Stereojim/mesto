6a5a9736f2b70099a0891cecae7f6edda7c2e54b
Stereojim <jimmysky@mail.ru>
2021-11-06 01:41:43 +0300

    fix: исправления по ревью - попап

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,42 +1,33 @@
 let popup = document.querySelector(".popup");
 let editButton = document.querySelector(".profile__edit-button");
 let closePopapButton = document.querySelector(".popup__button-close");
+let person = document.querySelector(".profile__name");
+let profession = document.querySelector(".profile__profession");
+let nameInput = document.querySelector(".input_name");
+let professionInput = document.querySelector(".input_profession");
+let submitForm = document.querySelector(".popup");
 
 function openPopap() {
   popup.classList.add("popup_opened");
+  let defaultName = person.textContent;
+  let defaulProfession = profession.textContent;
+  nameInput.value = defaultName;
+  professionInput.value = defaulProfession;
 }
 
 function closePopap() {
   popup.classList.remove("popup_opened");
 }
 
-editButton.addEventListener("click", openPopap);
-closePopapButton.addEventListener("click", closePopap);
-
-let person = document.querySelector(".profile__name");
-let profession = document.querySelector(".profile__profession");
-let nameInput = document.querySelector(".popup__input_name");
-let professionInput = document.querySelector(".popup__input_profession");
-
-document.querySelector(".popup__input_name").value = person.textContent;
-document.querySelector(".popup__input_profession").value =
-  profession.textContent;
-
-let submitForm = document.querySelector(".popup");
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
   let newName = nameInput.value;
   let newProfession = professionInput.value;
-
-  let person = document.querySelector(".profile__name");
-  let profession = document.querySelector(".profile__profession");
-
   person.textContent = newName;
   profession.textContent = newProfession;
-
   popup.classList.remove("popup_opened");
 }
 
 submitForm.addEventListener("submit", formSubmitHandler);
+editButton.addEventListener("click", openPopap);
+closePopapButton.addEventListener("click", closePopap);
