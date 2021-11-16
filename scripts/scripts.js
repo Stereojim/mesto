b1c3d7e384766746815b97ac887aaefc7115c1ff
Stereojim <jimmysky@mail.ru>
2021-11-16 11:21:09 +0300

    добавлена логика js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,6 +7,57 @@ let nameInput = document.querySelector("#name");
 let professionInput = document.querySelector("#profession");
 let submitForm = document.querySelector(".popup");
 
+let addPlaceButton = document.querySelector(".profile__add-button");
+let closePlaceAdd = document.querySelector("#closePlaceAdd");
+
+const places = document.querySelector(".elements");
+
+
+
+
+function addPlace(pictureLink, placeName) {
+  const placeTemplate = document.querySelector("#place-template").content;
+  const newPlace = placeTemplate.querySelector('.popup').cloneNode(true);
+  newPlace.querySelector('#pictureLink').src = pictureLink;
+  newPlace.querySelector('#placeName').textContent = placeName;
+  places.append(newPlace);
+
+  function cancelAddingPlace() {
+    newPlace.remove();
+  }
+  
+  let closePlaceAdd = document.querySelector("#closePlaceAdd");
+  closePlaceAdd.addEventListener("click", cancelAddingPlace);
+}
+  
+  //
+  //
+
+
+
+
+  //function closeAddPlacePopup () {
+  //  newPlace.classList.remove("popup_opened");
+  //}
+
+  
+
+
+
+/* SavePlaceButton.addEventListener('click', function () {
+  const placeLink = document.querySelector('#pictureLink');
+  const placeTitle = document.querySelector('#placeName');
+
+  addPlace(placeLink.value, placeTitle.value);
+  openPopap();
+
+  placeLink.value = '';
+  placeTitle.value = '';
+
+}); */
+
+
+
 function openPopap() {
   popup.classList.add("popup_opened");
   nameInput.value = person.textContent;
@@ -15,6 +66,10 @@ function openPopap() {
 
 function closePopap() {
   popup.classList.remove("popup_opened");
+}
+
+function closeAddPlacePopup () {
+  newPlace.classList.remove("popup_opened");
 }
 
 function formSubmitHandler(evt) {
@@ -28,4 +83,5 @@ function formSubmitHandler(evt) {
 
 submitForm.addEventListener("submit", formSubmitHandler);
 editButton.addEventListener("click", openPopap);
+addPlaceButton.addEventListener("click", addPlace);
 closePopapButton.addEventListener("click", closePopap);
