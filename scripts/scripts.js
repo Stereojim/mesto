665b369135e5552f755cf5b2a6552e158b8acf1a
Stereojim <jimmysky@mail.ru>
2021-11-21 22:34:51 +0300

    промежуточный коммит - надо удалять, все заново делать

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,19 @@
 const popup = document.querySelector(".popup");
 const editButton = document.querySelector(".profile__edit-button");
 const closePopapButton = document.querySelector(".popup__button-close");
-let person = document.querySelector(".profile__name");
-let profession = document.querySelector(".profile__profession");
+const person = document.querySelector(".profile__name");
+const profession = document.querySelector(".profile__profession");
 const nameInput = document.querySelector("#name");
 const professionInput = document.querySelector("#profession");
-let placeName = document.querySelector("#placeName");
-let pictureLink = document.querySelector("#pictureLink");
+const placeName = document.querySelector("placeName");
+const pictureLink = document.querySelector("#pictureLink");
 const submitForm = document.querySelector("#personCard");
 const addPlaceButton = document.querySelector(".profile__add-button");
 const closePlaceAdd = document.querySelector("#closePlaceAdd");
 const places = document.querySelector(".elements");
 const placeCard = document.querySelector("#placeCard").content;
 const placePopupTemplate = document.querySelector("#place-template");
+const showCard = document.querySelector("#showCard").content;
 
 const initialCards = [
   {
@@ -41,27 +42,23 @@ const initialCards = [
   },
 ];
 
+// рендеринг карточки из массива
 
-// прямое добавление в массив
+function createPlacesDomNode(item) {
+  const placeCardTemplate = placeCard.querySelector(".card").cloneNode(true);
+  placeCardTemplate.querySelector(".card__place-name").textContent = item.name;
+  placeCardTemplate.querySelector(".card__image").src = item.link;
 
-/* initialCards.unshift({
-  name: "мишка",
-  link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-
-}); */
-
-
-
-// рендеринг карточек из массива
-
-const createPlacesDomNode = (item) => {
-  const placeCardTemplate = placeCard.querySelector('.card').cloneNode(true);
-  placeCardTemplate.querySelector('.card__place-name').textContent = item.name;
-  placeCardTemplate.querySelector('.card__image').src = item.link;
+  function openPicture() {
+    showPicture.style.display = "flex";
+    bigPicture.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
 
   return placeCardTemplate;
-
 }
+
+// применение ко всему массиву
 
 const result = initialCards.map((item) => {
   return createPlacesDomNode(item);
@@ -69,86 +66,67 @@ const result = initialCards.map((item) => {
 
 places.append(...result);
 
-/* // альтернативное добавление карточек массива
 
-initialCards.forEach(function (element) {
-  const cardElement = placeCard.cloneNode(true);
 
-  cardElement.querySelector(".card__image").src = element.link;
-  cardElement.querySelector(".card__place-name").textContent = element.name;
-  cardElement.querySelector(".card__image").alt = element.name;
+  // кнопка открытия формы создания новой карточки места
 
-  places.append(cardElement);
-  
-}); */
+  addPlaceButton.addEventListener("click", function () {
+    addPlace();
+    document.getElementById("placePopup").classList.add("popup_opened");
+  });
 
 // добавление и открытие попапа для создания новой карточки
 
 function addPlace() {
   const placeTemplate = document.querySelector("#place-template").content;
   const newPlace = placeTemplate.querySelector(".popup").cloneNode(true);
- 
   places.append(newPlace);
 
   function cancelAddingPlace() {
     newPlace.remove();
   }
 
-  let closePlaceAdd = document.querySelector("#closePlaceAdd");
+  // кнопка закрытия формы создания новой карточки места
+
+  const closePlaceAdd = document.querySelector("#closePlaceAdd");
   closePlaceAdd.addEventListener("click", cancelAddingPlace);
 
+  // сохранение карточки с кнопками удаления и лайка
 
+  const placeSubmitFormHandler = (evt) => {
+    evt.preventDefault();
+    const inputPlaceName = document.querySelector("#placeName").value;
+    const inputPictureLink = document.querySelector("#pictureLink").value;
+    const cardString = createPlacesDomNode({
+      name: inputPlaceName,
+      link: inputPictureLink,
+    });
+
+    places.prepend(cardString);
+
+    // дубль кнопки удаления для нового объекта
+    const removeButton = document.getElementsByClassName("card__remove-button");
+    for (let i = 0; i < removeButton.length; i++) {
+      removeButton[i].addEventListener(
+        "click",
+        function (e) {
+          e.currentTarget.parentNode.remove();
+        },
+        false
+      );
+    }
+
+    // закрытие после сохранения
+    cancelAddingPlace();
+
+
+    // странно работающий лайк (через раз)
+    likeCardButton();
+  };
+
+    // кнопка создать
+  document.querySelector("#placePopup").addEventListener("submit", placeSubmitFormHandler);
 }
-
-const placeSubmitFormHandler = (evt) => {
-  evt.preventDefault();
-  alert("hello there!");
-}; 
-
-placePopupTemplate.addEventListener('submit', placeSubmitFormHandler);
-
-
-
-
-
-// сохранение карточки 
-
-/* const cardFormSubmitHandler = (evt) => {
-  evt.preventDefault();
-
-  const inputPlaceName = placeName.value;
-  const inputPistureLink = pictureLink.value;
-
-  const placeString = createHTMLString({ name: inputPlaceName}, { link: inputPistureLink});
-
-  places.append(placeString);
-
-  placeName.value = '';
-  pictureLink.value = '';
-
-}
-
-placePopupTemplate.addEventListener('submit', cardFormSubmitHandler);
-
- const createPlaceTemplate = (item) => {
-  const placeCardTemplate = placeCard.querySelector('.card').cloneNode(true);
-  placeCardTemplate.querySelector('#placeName').textContent = item.name;
-  placeCardTemplate.querySelector('#pictureLink').textContent = item.link;
-
-  return placeCardTemplate;
-
-}  */
-
-
-
-// document.querySelector('.place-submit').addEventListener('submit', placeSubmitFormHandler);
-
-
-
-// кнопки
-
-addPlaceButton.addEventListener("click", addPlace);
-
 
 
 // редактирование профиля
@@ -185,18 +163,44 @@ closePopapButton.addEventListener("click", closePopap);
 
 // кнопка удаления карточки
 
-let removeButton = document.getElementsByClassName('card__remove-button');
+const removeButton = document.getElementsByClassName("card__remove-button");
 for (let i = 0; i < removeButton.length; i++) {
-  removeButton[i].addEventListener('click', function(e) {
-    e.currentTarget.parentNode.remove();
-  }, false);
-};
+  removeButton[i].addEventListener(
+    "click",
+    function (e) {
+      e.currentTarget.parentNode.remove();
+    },
+    false
+  );
+}
+
+
+function likeCardButton() {
+  const likeButton = document.getElementsByClassName("card__like-button");
+  for (let i = 0; i < likeButton.length; i++) {
+    likeButton[i].addEventListener(
+      "click",
+      function (evt) {
+        evt.currentTarget.classList.toggle("card__like-button_active");
+      },
+      false
+    );
+  }
+}
 
 // лайк
 
-let likeButton = document.getElementsByClassName('card__like-button');
+/* const likeButton = document.getElementsByClassName("card__like-button");
 for (let i = 0; i < likeButton.length; i++) {
-  likeButton[i].addEventListener('click', function(e) {
-    e.currentTarget.classList.toggle("card__like-button_active");
-  }, false);
-}
+  likeButton[i].addEventListener(
+    "click",
+    function (evt) {
+      evt.currentTarget.classList.toggle("card__like-button_active");
+    },
+    false
+  );
+} */
+
+
+const DOM = document.querySelectorAll("*");
+console.log(DOM);
