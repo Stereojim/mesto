609444a6ea64c0f552a0b715e09f9aeb905fee26
Stereojim <jimmysky@mail.ru>
2021-11-22 22:56:09 +0300

    feat 90% function - cannot open newcard at fullsize

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,18 @@
-const popup = document.querySelector(".popup");
-const editButton = document.querySelector(".profile__edit-button");
-const closePopapButton = document.querySelector(".popup__button-close");
-const person = document.querySelector(".profile__name");
-const profession = document.querySelector(".profile__profession");
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closePopapButton = document.querySelector('.popup__button-close');
+const person = document.querySelector('.profile__name');
+const profession = document.querySelector('.profile__profession');
 const nameInput = document.querySelector("#name");
-const professionInput = document.querySelector("#profession");
-const placeName = document.querySelector("placeName");
-const pictureLink = document.querySelector("#pictureLink");
-const submitForm = document.querySelector("#personCard");
-const addPlaceButton = document.querySelector(".profile__add-button");
-const closePlaceAdd = document.querySelector("#closePlaceAdd");
-const places = document.querySelector(".elements");
-const placeCard = document.querySelector("#placeCard").content;
-const placePopupTemplate = document.querySelector("#place-template");
+const professionInput = document.querySelector('#profession');
+const placeName = document.querySelector('placeName');
+const pictureLink = document.querySelector('#pictureLink');
+const submitForm = document.querySelector('#personCard');
+const addPlaceButton = document.querySelector('.profile__add-button');
+const closePlaceAdd = document.querySelector('#closePlaceAdd');
+const places = document.querySelector('.elements');
+const placeCard = document.querySelector('#placeCard').content;
+const placePopupTemplate = document.querySelector('#place-template');
 
 const initialCards = [
   {
@@ -54,20 +54,22 @@ function createPlacesDomNode(item) {
 
 // применение ко всему массиву
 
+
 const result = initialCards.map((item) => {
   return createPlacesDomNode(item);
 });
 
 places.append(...result);
 
-/* likeCardButton(); */
 
-// кнопка открытия формы создания новой карточки места
+  function cancelAddingPlace() {
+    newPlace.remove();
+  }
 
-addPlaceButton.addEventListener("click", function () {
-  addPlace();
-  document.getElementById("placePopup").classList.add("popup_opened");
-});
+  addPlaceButton.addEventListener("click", function () {
+    addPlace();
+    document.getElementById("placePopup").classList.add("popup_opened");
+  });
 
 // добавление и открытие попапа для создания новой карточки
 
@@ -96,7 +98,9 @@ function addPlace() {
       link: inputPictureLink,
     });
 
+  
     places.prepend(cardString);
+
 
     // дубль кнопки удаления для нового объекта
     const removeButton = document.getElementsByClassName("card__remove-button");
@@ -122,11 +126,25 @@ function addPlace() {
     });
   };
 
+
+// кнопка открытия формы создания новой карточки места
+
+
+
+
+
+
   // кнопка создать
   document
-    .querySelector("#placePopup")
+    .querySelector('.popup')
     .addEventListener("submit", placeSubmitFormHandler);
 }
+
+
+
+
+
+
 
 // редактирование профиля
 // открытие формы для редактирования профиля
@@ -173,53 +191,11 @@ for (let i = 0; i < removeButton.length; i++) {
   );
 }
 
-// лайки
-// функция
 
-/* function likeCardButton() {
-  const likeButton = document.getElementsByClassName("card__like-button");
-  for (let i = 0; i < likeButton.length; i++) {
-    likeButton[i].addEventListener(
-      "click",
-      function (evt) {
-        evt.currentTarget.classList.toggle("card__like-button_active");
-      },
-      false
-    )
-  }
-}; */
-
-// объект
-
-/* const likeButton = document.getElementsByClassName("card__like-button");
-for (let i = 0; i < likeButton.length; i++) {
-  likeButton[i].addEventListener(
-    "click",
-    function (evt) {
-      evt.currentTarget.classList.toggle("card__like-button_active");
-    },
-    false
-  );
-} */
-
-// лайк по практикуму
-
-/* likeButton.addEventListener('click', function (evt) {
-  const eventTarget = evt.target;
-  eventTarget.classList.toggle('card__like-button_active');
-  }); */
+// просмотр dom
 
 const DOM = document.querySelectorAll("*");
 console.log(DOM);
-
-/* openPicture = document.getElementById('show');
-picture = document.getElementById('card');
-
-picture.addEventListener('click', function (evt) {
-const picture = evt.target;
-picture.classList.toggle('show') 
-}
-); */
 
 // лайк поиск по всем (работает!)
 
@@ -231,97 +207,41 @@ const like = document.querySelectorAll(".card__like-button");
   };
 });
 
-/* const originPicture = document.querySelector('.card__image');
-[].forEach.call( originPicture, function(el) {
-  el.onclick = function(e) {
-  pictureView = e.target;
-  pictureView = showCard.querySelector(".show").cloneNode(true);
-  pictureView.querySelector(".show__title").textContent = this.name;
-  pictureView.querySelector(".show__picture").src = this.src;
-  pictureView.querySelector(".show__picture").alt = this.name;
-  return pictureView;
-  }
-}); */
-
+// открытие модального окна с фоткой и подписью
 
 window.onload = function () {
-	const imgArr = document.getElementsByClassName('card__image');
-  const modalWindow = document.querySelector('.show');
-  const modalImg = document.querySelector('.show__picture');
-  const caption = document.querySelector('.show__title');
-  const closeImg = document.querySelector('.show__close-button')
+  const imgArr = document.getElementsByClassName("card__image");
+  const modalWindow = document.querySelector(".show");
+  const modalImg = document.querySelector(".show__picture");
+  const caption = document.querySelector(".show__title");
+  const closeImg = document.querySelector(".show__close-button");
 
-  for(i = 0; i<imgArr.length; i++) {
+  // поиск места клика
+
+  for (i = 0; i < imgArr.length; i++) {
     const picture = imgArr[i];
-    picture.onclick = function() {
+    picture.onclick = function () {
       openImg(this);
-    }
+    };
   }
 
+  // открытие окна + прописывание src и title
+
   function openImg(pic) {
-    modalWindow.style.display = 'flex';
+    modalWindow.style.display = "flex";
     modalImg.src = pic.src;
     modalImg.alt = pic.alt;
     caption.innerHTML = pic.alt;
   }
 
+  // скрытие модального окна
+
   function close() {
-    modalWindow.style.display = 'none';
+    modalWindow.style.display = "none";
   }
 
-  closeImg.onclick = function() {
-    modalWindow.style.transform = 'transition(opacity ease 5s)'
-    setTimeout( close, 100);
-  }
-}
-
-
-
-
-
-/* const showCard = document.querySelector(".show");
-
-function openBigPicture(event) {
-  showCard.classList.add("show_opened");
-
-const title = event.currentTarget.dataset('.card__name').textContent;
-const link = document.querySelector('.card__image').src;
-
-  showCard.querySelector(".show__title").textContent = event.title;
-  showCard.querySelector(".show__picture").src = event.src;
-  showCard.querySelector(".show__picture").alt = title;
-
-} */
-
-
-
-/*   const curPic  = evt.target.dataset.btn;
-if (curPic === 'pic') {
-  console.log('эта карточка');
-}
-}; */
-
-
-
-/*   showCard.querySelector(".show__title").textContent = curPic.querySelector('.card__place-name').textContent;
-  showCard.querySelector(".show__picture").src =
-    "https://images.unsplash.com/photo-1626224574073-23989ea25ae8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=710&q=80";
-  showCard.querySelector(".show__picture").alt = "this.name";
-  return showCard;
-} */
-
-document.addEventListener("click", (event) => {
-  /* event.preventDefault(); */
-  const picType = event.target.dataset.btn;
-
-  if (picType === "pic") {
-    openBigPicture();
-  }
-});
-
-function closePicture() {
-  showCard.classList.remove("show_opened");
-}
-
-const closePictureButton = document.querySelector(".show__close-button");
-closePictureButton.addEventListener("click", closePicture);
+  closeImg.onclick = function () {
+    modalWindow.style.transform = "transition(opacity ease 5s)";
+    setTimeout(close, 100);
+  };
+};
